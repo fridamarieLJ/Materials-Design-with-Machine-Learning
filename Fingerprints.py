1eaf7910d0302c71670dfe4e42ca13cc839f15a0
fridamarieLJ <85923902+fridamarieLJ@users.noreply.github.com>
2025-01-20 23:49:09 +0100

    Update Fingerprints.py

--- Fingerprints.py
+++ Fingerprints.py
@@ -1,12 +1,23 @@
 import numpy as np
+import os
 import pandas as pd
+import json
 from dscribe.descriptors import CoulombMatrix
+from dscribe.descriptors import MBTR
 from ase import Atoms
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import matplotlib
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from scipy.spatial.distance import cdist
+from scipy.optimize import differential_evolution
+import matplotlib.pyplot as plt
 
 # Coulomb Matrices
 # Setting up the CM descriptor
 def Coulombmatrices(train, max_number_of_atoms):
-    cm = CoulombMatrix(n_atoms_max=max_number_of_atoms, permutation = 'sorted_l2')
+    cm = CoulombMatrix(n_atoms_max=max_number_of_atoms)
 
     cmats = np.zeros((len(train),max_number_of_atoms**2))
     for i,atoms in enumerate(train.atoms):
@@ -15,4 +26,3 @@ def Coulombmatrices(train, max_number_of_atoms):
         cmats[i,:] = cm.create(atoms)
     
     return cmats
-
