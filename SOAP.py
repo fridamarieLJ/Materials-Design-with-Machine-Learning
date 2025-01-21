112651c839bbbf1644e9b34f452130f4a515461b
fridamarieLJ <fridamarielundjep@gmail.com>
2025-01-21 12:39:32 +0100

    update soap

--- SOAP.py
+++ SOAP.py
@@ -27,6 +27,7 @@ def SOAPfingerprints(train, species, r_cut, n_max, l_max, sigma, periodic, avera
         l_max=l_max,
         sigma=sigma,
         periodic=periodic
+        average = average
     )
 
     # Pre-allocate storage for SOAP descriptors
