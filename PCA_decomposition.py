4bea82236dd37b5bf2284aca33087c8bcffd8bef
AlbertRiber <albertlriber@gmail.com>
2025-01-21 16:21:23 +0100

    Update PCA_decomposition.py

--- PCA_decomposition.py
+++ PCA_decomposition.py
@@ -24,12 +24,12 @@ def PCA_decomposition(X_train, X_test, number_of_PCs):
     pca = PCA(n_components=number_of_PCs) 
     X_train_pca = pca.fit_transform(X_train_scaled)
     print("With {} PCA components {var:0.4f}% of the variance is explained".format(number_of_PCs, var = 100*np.sum(pca.explained_variance_ratio_)))
-    print('X_train: {}'.format(X_train.shape))
+    print('X_train_pca: {}'.format(X_train_pca.shape))
 
     # Now apply the same transformations to the test set
     X_test_scaled = scaler.transform(X_test)  # Use the same scaler
     X_test_pca = pca.transform(X_test_scaled)  # Use the same PCA
 
-    print('X_test: {}'.format(X_test.shape))
+    print('X_test_pca: {}'.format(X_test_pca.shape))
 
     return X_train_pca, X_test_pca
